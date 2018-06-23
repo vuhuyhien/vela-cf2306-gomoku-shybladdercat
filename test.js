100d6a7ab0741616331785298929cdcbb3db9356
vuhuyhien <vuhuyhien@velacorp.vn>
2018-06-23 14:51:16 +0700

    design test case

---
+++ test.js
@@ -0,0 +1,51 @@
+var should = require('chai').should()
+
+describe('Chỉ giới hạn hai người chơi', function () {
+    it('Số người chơi được lưu trong game là 2', function () {
+        
+    });
+});
+
+describe('Chức năng chọn người đi trước', function () {
+    /**
+     * Chọn radio button người đi trước
+     * Ấn nút start game
+     */
+    it('Màn hình game hiển thị tên người đi trước', function () {
+        
+    });
+});
+
+describe('Người đi trước thực hiện nước đi', function () {
+    it('Không được đi vào ô đã chọn', function () {
+        
+    });
+
+    it('Nước đi của người chơi được lưu lại', function () {
+        
+    });
+
+    it('Người chơi hiện tại chuyển thành người chơi còn lại', function () {
+        
+    });
+});
+
+
+describe('Ván chơi kết thúc khi có 5 ô sát nhau được đánh dấu giống nhau', function () {
+    it('Ván đấu kết thúc 5 ô liên tiếp theo hàng ngang', function () {
+        
+    });
+
+    it('Ván đấu kết thúc 5 ô liên tiếp theo hàng chéo', function () {
+        
+    });
+
+    it('Ván đấu kết thúc 5 ô liên tiếp theo hàng dọc', function () {
+        
+    });
+
+    it('hiển thị kết quả', function () {
+        
+    });
+});
+
